52f7c581d81e53265e328e05ca67037f15ccd1ae
Cam Crockett <cam.crockett@localhost>
2015-12-11 10:32:03 -0800

    Adding setup steps to step 00 and updating the environmentVariables.sh to reflect the new user options

--- NodeJS_Workshop/Step00_setup/environmentVariables.sh
+++ NodeJS_Workshop/Step00_setup/environmentVariables.sh
@@ -3,10 +3,10 @@ echo "Creating Environment Variables: execute as $source environmentVariables.sh
 export PORT=8090
 echo "PORT set as " $PORT
 
-export DBAAS_DEFAULT_CONNECT_DESCRIPTOR="129.152.151.225:1521/PDB1.jcsdemo248.oraclecloud.internal"
+export DBAAS_DEFAULT_CONNECT_DESCRIPTOR="<IP GOES HERE>/<YOUR PLUGGABLE DB CONNECITON STRING>"
 echo "DBAAS_DEFAULT_CONNECT_DESCRIPTOR set as " $DBAAS_DEFAULT_CONNECT_DESCRIPTOR
 
-export DBAAS_USER_NAME="system"
+export DBAAS_USER_NAME="devuser"
 echo "DBAAS_USER_NAME set as " $DBAAS_USER_NAME
 
 export DBAAS_USER_PASSWORD="Welcome1#"
