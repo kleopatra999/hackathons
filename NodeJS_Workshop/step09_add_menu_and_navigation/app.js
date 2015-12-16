43e1d1d2a84b1651cb6b998aa20a5c3e9c31f44f
ccrock <cam.crockett.12@gmail.com>
2015-12-16 08:50:47 -0700

    Updating Formatting

--- NodeJS_Workshop/step09_add_menu_and_navigation/app.js
+++ NodeJS_Workshop/step09_add_menu_and_navigation/app.js
@@ -1,36 +1,26 @@
 'use strict'
 
-var app = angular
-  .module('aOne', ['ngRoute'])
-  
+var app = angular.module('aOne', [ 'ngRoute' ])
 
-app.controller('browseController', function($http,$scope){
+app.controller('browseController', function($http, $scope) {
 
-var itemStatus= $http.get('/item').then(function(response, err){
+	var itemStatus = $http.get('/item').then(function(response, err) {
 
-	if(err) { console.log('error is', err)} 
-	console.log('data returned',response)
-	$scope.items = response.data
-})
+		if (err) {
+			console.log('error is', err)
+		}
+		console.log('data returned', response)
+		$scope.items = response.data
+	})
 
-console.log('returned', itemStatus)
-
-
+	console.log('returned', itemStatus)
 
 });
 
-app.config(function ($routeProvider) {
-    $routeProvider      
-      .when('/', {
-        templateUrl: './browse.html',
-        controller: 'browseController'     
-      })
-
-	
-
+app.config(function($routeProvider) {
+	$routeProvider.when('/', {
+		templateUrl : './browse.html',
+		controller : 'browseController'
+	})
 
 })
-
-
-
-
