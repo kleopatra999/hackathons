633c257869d337ac2d690dff1cce685414d9af61
ccrock <cam.crockett.12@gmail.com>
2015-12-22 10:20:30 -0700

    Re-Numbering some more. Removed step 17

--- NodeJS_Workshop/step17_Make_an_offer/scripts/dbconfig.js
+++
@@ -1,9 +0,0 @@
-var connectionStr = process.env.DBAAS_DEFAULT_CONNECT_DESCRIPTOR;
-var username = process.env.DBAAS_USER_NAME;
-var password = process.env.DBAAS_USER_PASSWORD;
-
-module.exports= {
-  user:username,
-  password:password,
-  connectString : connectionStr
-};
